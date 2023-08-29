83425627b6b3dc856dad09e30a964ea2cf76be4e
Joshua Emore <ehimareemore@gmail.com>
2023-08-29 22:20:17 +0100

    Masures balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,6 +6,25 @@
  *
  * Return: 0 or depth.
 */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (!tree->left && !tree->right)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height;
