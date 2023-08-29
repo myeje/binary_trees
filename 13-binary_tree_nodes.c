21c2f8681f6e75a27bd2f80ded5b1911a6bc2569
Joshua Emore <ehimareemore@gmail.com>
2023-08-29 21:44:22 +0100

    Counts the nodes with at least 1 child

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: 0 or depth.
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL || tree->right == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+	}
+}
