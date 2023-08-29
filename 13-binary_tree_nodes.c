d586549e0ff45d803f462a80a4f81f32c38ec753
Joshua Emore <ehimareemore@gmail.com>
2023-08-29 21:47:28 +0100

    Counts the nodes with at least 1 child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,12 +13,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
-		return (0);
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 	}
 	else
 	{
-		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+		return (0);	
 	}
 }
