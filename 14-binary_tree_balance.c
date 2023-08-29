b03e3e6afd6c39b6c8cb92d3c6dc1ccfad25044c
Joshua Emore <ehimareemore@gmail.com>
2023-08-29 22:08:02 +0100

    Masures balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: 0 or depth.
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+
+	int left = binary_tree_height(tree->left);
+	int right = binary_tree_height(tree->right);
+
+	return (left - right);	
+}
