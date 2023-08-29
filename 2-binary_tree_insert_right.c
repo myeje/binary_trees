3d88391e817d1cb650ffd09671fcc3c181642bcb
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-29 04:24:21 -0700

    binary_tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,6 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *ptr;
-	binary_tree_t *tmp;
 
 	ptr = malloc(sizeof(binary_tree_t));
 	if (!ptr || !parent)
@@ -18,18 +17,20 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 	ptr->n = value;
+	ptr->parent = parent;
 	ptr->left = NULL;
-	ptr->right = NULL;
 
-	if (parent->right != NULL)
+	if (parent->right)
 	{
-		tmp = parent->right;
-		parent->right = ptr;
-		ptr->right = tmp;
-		tmp->parent = ptr;
+		ptr->right = parent->right;
+		parent->right->parent = ptr;
 	}
 	else
-		parent->right = ptr;
+	{
+		ptr->right = NULL;
+	}
+	parent->right = ptr;
+	
 
 	return (ptr);
 }
