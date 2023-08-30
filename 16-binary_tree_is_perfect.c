a5d8b00fa4bfcf961d94caddd78f9bbfa8c01201
Joshua Emore <ehimareemore@gmail.com>
2023-08-30 01:24:51 +0100

    fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,7 +37,7 @@ size_t binary_tree_count_nodes(const binary_tree_t *tree)
 {
     if (tree == NULL)
 	{
-        return 0;
+		return (0);
     }
 
     return (binary_tree_count_nodes(tree->left) + binary_tree_count_nodes(tree->right) + 1);
