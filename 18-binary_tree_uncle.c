a9e8ba5f346ae766f9a59ec9a4fc6b3f0f3d2e86
Joshua Emore <ehimareemore@gmail.com>
2023-08-30 14:04:14 +0100

    Finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,23 +6,18 @@
  *
  * Return: NULL or uncle node.
 */
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-	binary_tree_t *parent;
+binary_tree_t *binary_tree_uncle(binary_tree_t *node) {
+    if (node == NULL || node->parent == NULL || node->parent->parent == NULL) {
+        return NULL;
+    }
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-	{
-		return (NULL);
-	}
+    binary_tree_t *grandparent = node->parent->parent;
 
-	parent = node->parent->parent;
-	if (parent->left->left == node || parent->left->right == node)
-	{
-		return (parent->right);
-	}
-	else if (parent->right->right == node || parent->right->left == node)
-	{
-		return (parent->left);
-	}
-	return (NULL);
+    if (grandparent->left == node->parent) {
+        return grandparent->right;
+    } else if (grandparent->right == node->parent) {
+        return grandparent->left;
+    }
+
+    return NULL;
 }
