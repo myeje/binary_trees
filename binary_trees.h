aa0c8b8720c9be4395cc71e1e7c41e7978fd6736
Joshua Emore <ehimareemore@gmail.com>
2023-08-30 01:29:31 +0100

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -41,7 +41,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-size_t binary_tree_count_nodes(const binary_tree_t *tree);
+size_t binary_tree_count(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
