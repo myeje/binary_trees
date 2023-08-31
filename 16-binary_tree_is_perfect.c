b5a166e44d0a443f85c74e1af4c7a34666005852
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-08-31 10:22:39 -0700

    changes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-#include "binary_trees.h"
-
 /**
  * binary_tree_height - measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
@@ -17,9 +15,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-
-	if (!tree->left && !tree->right)
-		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
