7dcb88c59002a06bfabee1f0d999ab9b35fad260
Joshua Emore <ehimareemore@gmail.com>
2023-08-31 17:24:49 +0100

    emoves a node from AVL tree

---
+++ 123-avl_remove.c
@@ -0,0 +1,77 @@
+#include "binary_trees.h"
+
+/**
+ * avl_balance - Balances an AVL tree after insertions or deletions.
+ * @node: A pointer to the root node of the tree.
+ *
+ * Return: A pointer to the new root node after balancing
+ */
+avl_t *avl_balance(avl_t *node)
+{
+    int balance_factor = binary_tree_balance(node);
+
+    if (balance_factor > 1) {
+        if (binary_tree_balance(node->left) >= 0)
+            return (binary_tree_rotate_right(node));
+        else {
+            node->left = binary_tree_rotate_left(node->left);
+            return (binary_tree_rotate_right(node));
+        }
+    }
+
+    if (balance_factor < -1) {
+        if (binary_tree_balance(node->right) <= 0)
+            return (binary_tree_rotate_left(node));
+        else {
+            node->right = binary_tree_rotate_right(node->right);
+            return (binary_tree_rotate_left(node));
+        }
+    }
+
+    return (node);
+}
+
+/**
+ * avl_remove - this function removes a node from an AVL tree
+ * @root: pointer to the root node of tree to remove
+ * @value: value to remove in the tree
+ *
+ * Return: pointer to the new root node of the tree
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+	avl_t *sub = NULL;
+
+	if (!root)
+	{
+		return (NULL);
+	}
+
+	if (value > root->n)
+	{
+		root->right = avl_remove(root->right, value);
+	}
+	else if (value < root->n)
+	{
+		root->left = avl_remove(root->left, value);
+	}
+	else
+	{
+		if (!root->right || root->left)
+		{
+			sub = root->left ? root->left : root->right;
+			free(root);
+			return sub;
+		}
+		sub = root->right;
+		while (sub->left)
+		{
+			sub = sub->left;
+		}
+		root->n = sub->n;
+		root->right = avl_remove(root->right, sub->n);
+	}
+
+	root = avl_balance(root);
+	return (root);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -63,6 +63,8 @@ bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *avl_insert(avl_t **tree, int value);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *avl_balance(avl_t *node);
 int heap_extract(heap_t **root);
 
 /*Print function prototype*/
